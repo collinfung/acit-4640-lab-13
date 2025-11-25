2884239531dd5c2309f5d271d39739b7cf7428ce
collin <fungcollin@gmail.com>
2025-11-25 14:50:17 -0800

    Save local changes before pulling remote images

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,16 @@
 terraform {
+  backend "s3" {
+    bucket         = "cf-terraform-s3-backend"
+    key            = "terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+    use_lockfile   = true
+  }
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
   }
@@ -10,4 +19,3 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
-
