f5ffa09ca59c2c6159e99467d73e84e520d94886
Collin Fung <fungcollin@gmail.com>
2025-11-25 17:10:16 -0800

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,7 +4,6 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
-    dynamodb_table = "terraform-locks"
     use_lockfile   = true
   }
 
